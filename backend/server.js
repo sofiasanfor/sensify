0c43f0a67a5b678b8d53195ac98f977f171e0900
Sofia Sanchez <sofia.sanchez.forero@gmail.com>
2026-04-02 12:24:27 -0500

    fix error 3

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static(path.join(__dirname,"uploads")))
 
 // CONEXION
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("Mongo conectado");
 
